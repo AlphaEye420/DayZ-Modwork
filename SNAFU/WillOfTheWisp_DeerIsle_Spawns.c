cc9ae9b57c41bfae34a79056f82bdfa85176999e
AlphaEye <124981604+AlphaEye420@users.noreply.github.com>
2023-08-23 18:41:13 -0400

    Will of the Wisp DeerIsle Spawns
    for intt.c

---
+++ SNAFU/WillOfTheWisp_DeerIsle_Spawns.c
@@ -0,0 +1,31 @@
+// WILL OF WISP SPAWNS FOR DEER ISLE
+// add this to Main()
+
+    ab_Willowisp_Stealable_Items.Clear();
+    ab_Willowisp_Stealable_Items.Insert("NVGoggles");
+    ab_Willowisp_Stealable_Items.Insert("Lithium");
+    ab_Willowisp_Stealable_Items.Insert("CarbonZinc");
+    ab_Willowisp_Stealable_Items.Insert("battery9v");
+    ab_Willowisp_Stealable_Items.Insert("expansioncodelock");
+    ab_Willowisp_Stealable_Items.Insert("powergenerator");
+    ab_Willowisp_Stealable_Items.Insert("compass");
+    ab_Willowisp_Stealable_Items.Insert("orienteeringcompass");
+    ab_Willowisp_Stealable_Items.Insert("expansiongps");
+    ab_Willowisp_Stealable_Items.Insert("personalradio");
+    ab_Willowisp_Stealable_Items.Insert("batterycharger");
+    ab_Willowisp_Stealable_Items.Insert("megaphone");
+    ab_Willowisp_Stealable_Items.Insert("rangefinder");
+    ab_Willowisp_Stealable_Items.Insert("TF_AK_Tactica_Tula10000Receiver");
+    ab_Willowisp_Stealable_Items.Insert("TF_AK_B33Receiver");
+    ab_Willowisp_Stealable_Items.Insert("TF_NVGoggles_AVS9");
+    ab_Willowisp_Stealable_Items.Insert("TF_NVGoggles_PNV10T");
+    ab_Willowisp_Stealable_Items.Insert("TF_NVGoggles_PVS15");
+    ab_Willowisp_Stealable_Items.Insert("TF_NVGoggles_PVS14");
+    Get_ab_WillowispManager().CreateRandomWillowispController("Swanville Valley", "6239.62 236.655 11819.9", 333, 111, Math.RandomIntInclusive(1, 2));
+    Get_ab_WillowispManager().CreateRandomWillowispController("Baren Forest", "7007.19 74.6015 10498.1", 444, 111, 1);
+    Get_ab_WillowispManager().CreateRandomWillowispController("Northeast Island", "9941.9 50.812 15109.7", 777, 111, Math.RandomIntInclusive(1, 3));  	
+    Get_ab_WillowispManager().CreateRandomWillowispController("Power Plant", "4701.16 20.4425 7020.42", 222, 111, Math.RandomIntInclusive(1, 2));	
+    Get_ab_WillowispManager().CreateRandomWillowispController("Prison", "6411.83 25.5953 944.895", 333, 111, 1);
+    Get_ab_WillowispManager().CreateRandomWillowispController("LightHouse East", "11210.6 10.7891 3800.71", 222, 55, 1);
+    Get_ab_WillowispManager().CreateRandomWillowispController("Windmill West", "2110 2 6764", 444, 111, Math.RandomIntInclusive(1, 3));
+    Get_ab_WillowispManager().CreateRandomWillowispController("Laboratory", "15715.5996 16.5552 4475.47998", 222, 77, Math.RandomIntInclusive(1, 2));
